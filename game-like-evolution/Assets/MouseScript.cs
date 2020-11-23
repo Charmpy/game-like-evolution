4c363a66984d25924b916a5b6a026c7984d5ddec
Charmpy <alex-cvc@mail.ru>
2020-11-23 18:47:15 +0500

    MC camera logic

---
+++ game-like-evolution/Assets/MouseScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+
+    public float mouseSencetivity = 100f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSencetivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSencetivity * Time.deltaTime;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        
+    }
+}
