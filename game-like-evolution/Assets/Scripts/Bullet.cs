5fa1be96f930a4cf976409d56069424a950624dc
Charmpy <alex-cvc@mail.ru>
2020-11-24 20:51:15 +0500

    Bullet Class

---
+++ game-like-evolution/Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float lifeDuration = 2f;
+
+    private float lifeTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTimer = lifeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Make the bullet move
+        transform.position += transform.forward * speed * Time.deltaTime;
+        // check timer
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0f)
+        {
+            Destroy (gameObject);
+        }
+    }
+
+}
---
+++ game-like-evolution/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public Camera playerCamera;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletObject = Instantiate (bulletPrefab);
+            bulletObject.transform.position = playerCamera.transform.position + playerCamera.transform.forward * 2f;
+            bulletObject.transform.forward = playerCamera.transform.forward;
+        }
+    }
+}
